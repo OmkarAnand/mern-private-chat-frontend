723c6047de31729a13c855a8a39386567fd99b0e
OmkarAnand <tutu.823204@gmail.com>
2025-10-18 22:58:28 +0530

    Updated CORS and environment variables

--- src/socket.js
+++ src/socket.js
@@ -2,9 +2,11 @@ import { io } from "socket.io-client";
 
 const socket = io(process.env.REACT_APP_API_URL, {
   transports: ["websocket"],
-  auth: {
-    token: localStorage.getItem("token") // send JWT for auth
-  }
+  withCredentials:true,
+//   auth: {
+//     token: localStorage.getItem("token") // send JWT for auth
+//   }
+
 });
 
 export default socket;
